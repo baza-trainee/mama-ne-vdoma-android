d80bd80379098323348925cb3db153309c54f4d0
o.krasnonozhenko <o.krasnonozhenko@andersenlab.com>
2023-10-02 07:29:47 +0300

    Request focus on OTP field when entering the screen

--- app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/ui/composables/OtpInputField.kt
+++ app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/ui/composables/OtpInputField.kt
@@ -15,8 +15,16 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
@@ -36,8 +44,21 @@ fun OtpTextField(
     onOtpTextChange: (String, Boolean) -> Unit = { _,_ -> }
 ) {
 
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    var textFieldLoaded by remember { mutableStateOf(false) }
+
     BasicTextField(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester)
+            .onGloballyPositioned {
+                if (!textFieldLoaded) {
+                    focusRequester.requestFocus()
+                    keyboardController?.show()
+                    textFieldLoaded = true
+                }
+            },
         value = TextFieldValue(otpText, selection = TextRange(otpText.length)),
         onValueChange = {
             if (it.text.length <= otpCount) {
