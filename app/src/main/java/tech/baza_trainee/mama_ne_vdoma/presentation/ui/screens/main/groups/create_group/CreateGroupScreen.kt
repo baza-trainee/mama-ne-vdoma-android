15cddc7abc6fedc3271ec346a1f07e0392266473
o.krasnonozhenko <o.krasnonozhenko@andersenlab.com>
2023-10-15 19:39:42 +0300

    Add imePadding to CreateGroupScreen

--- app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/ui/screens/main/groups/create_group/CreateGroupScreen.kt
+++ app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/ui/screens/main/groups/create_group/CreateGroupScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -79,6 +80,7 @@ fun CreateGroupScreen(
     Column(
         modifier = Modifier
             .verticalScroll(rememberScrollState())
+            .imePadding()
     ) {
         Spacer(modifier = Modifier.height(16.dp))
 
