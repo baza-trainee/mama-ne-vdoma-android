1198d745514b5e48f63fc52ce6cf1fa4b5a87d47
o.krasnonozhenko <o.krasnonozhenko@andersenlab.com>
2023-10-29 09:33:21 +0200

    Increase text spaces in PrivacyPolicyBlock

--- app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/ui/composables/custom_views/PrivacyPolicyBlock.kt
+++ app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/ui/composables/custom_views/PrivacyPolicyBlock.kt
@@ -7,18 +7,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import tech.baza_trainee.mama_ne_vdoma.presentation.ui.composables.functions.getTextWithUnderline
 import tech.baza_trainee.mama_ne_vdoma.presentation.ui.theme.redHatDisplayFontFamily
 
 @Composable
+@Preview
 fun PrivacyPolicyBlock(
     modifier: Modifier = Modifier,
     isChecked: Boolean = false,
@@ -47,7 +50,9 @@ fun PrivacyPolicyBlock(
             Spacer(modifier = Modifier.height(4.dp))
 
             Text(
-                modifier = Modifier.clickable {
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .clickable {
                     val uri = "https://mommy-not-home.online/privacy"
                     uriHandler.openUri(uri)
                 },
@@ -60,10 +65,10 @@ fun PrivacyPolicyBlock(
                 fontFamily = redHatDisplayFontFamily
             )
 
-            Spacer(modifier = Modifier.height(4.dp))
-
             Text(
-                modifier = Modifier.clickable {
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .clickable {
                     val uri = "https://mommy-not-home.online/terms"
                     uriHandler.openUri(uri)
                 },
@@ -76,10 +81,10 @@ fun PrivacyPolicyBlock(
                 fontFamily = redHatDisplayFontFamily
             )
 
-            Spacer(modifier = Modifier.height(4.dp))
-
             Text(
-                modifier = Modifier.clickable {
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .clickable {
                     val uri = "https://mommy-not-home.online/refusal"
                     uriHandler.openUri(uri)
                 },
