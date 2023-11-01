2a6f3c1294ce9f7202cf881c8a32ec928eeb18c3
o.krasnonozhenko <o.krasnonozhenko@andersenlab.com>
2023-11-01 09:50:59 +0200

    Fix camera shoot rotation

--- app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/utils/BitmapHelper.kt
+++ app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/utils/BitmapHelper.kt
@@ -5,8 +5,11 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.Matrix
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.net.Uri
 import android.util.Base64
+import android.view.OrientationEventListener
 import androidx.core.graphics.applyCanvas
 import androidx.core.net.toUri
 import androidx.exifinterface.media.ExifInterface
@@ -18,6 +21,7 @@ import okhttp3.ResponseBody
 import java.io.ByteArrayOutputStream
 import java.io.File
 
+
 class BitmapHelper(private val context: Context) {
 
     fun encodeToBase64(bitmap: Bitmap): String {
@@ -28,18 +32,12 @@ class BitmapHelper(private val context: Context) {
         return Base64.encodeToString(byteArray, Base64.DEFAULT)
     }
 
-    fun getSize(bitmap: Bitmap): Int {
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, PNG_QUALITY, byteArrayOutputStream)
-        return byteArrayOutputStream.toByteArray().size
-    }
-
     fun resizeBitmap(
         scope: CoroutineScope,
         image: Bitmap,
         onSuccess: (Bitmap) -> Unit,
         onError: () -> Unit
-        ) {
+    ) {
         if (image != DEFAULT_BITMAP) {
             scope.launch(Dispatchers.IO) {
                 val newImage = if (image.height > IMAGE_HEIGHT)
@@ -83,12 +81,14 @@ class BitmapHelper(private val context: Context) {
         return withContext(Dispatchers.IO) {
             try {
                 context.contentResolver.openInputStream(selectedImage)?.let {
-                    val img =  BitmapFactory.decodeStream(it)
-                    val orientation = ExifInterface(it).getAttributeInt(
-                        ExifInterface.TAG_ORIENTATION,
-                        ExifInterface.ORIENTATION_NORMAL
-                    )
-                    return@withContext when(orientation) {
+                    val deviceOrientation = context.resources.configuration.orientation
+                    val cameraManager =
+                        context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+                    val frontCamera = getFrontCameraId(cameraManager).orEmpty()
+                    val characteristics = cameraManager.getCameraCharacteristics(frontCamera)
+                    val orientation = getJpegOrientation(characteristics, deviceOrientation)
+                    val img = BitmapFactory.decodeStream(it)
+                    return@withContext when (orientation) {
                         ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(img, 90)
                         ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(img, 180)
                         ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(img, 270)
@@ -100,6 +100,47 @@ class BitmapHelper(private val context: Context) {
             }
             return@withContext DEFAULT_BITMAP
         }
+    }
+
+    private fun getSize(bitmap: Bitmap): Int {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, PNG_QUALITY, byteArrayOutputStream)
+        return byteArrayOutputStream.toByteArray().size
+    }
+
+    private fun getFrontCameraId(cameraManager: CameraManager?): String? {
+        cameraManager?.cameraIdList?.forEach {
+            val characteristics = cameraManager.getCameraCharacteristics(it)
+            val cameraFacing = characteristics.get(CameraCharacteristics.LENS_FACING)
+
+            if (cameraFacing == CameraCharacteristics.LENS_FACING_FRONT) {
+                return it
+            }
+        }
+
+        // Front camera not found
+        return null
+    }
+
+    private fun getJpegOrientation(
+        cameraCharacteristics: CameraCharacteristics,
+        deviceOrientation: Int
+    ): Int {
+        var _deviceOrientation = deviceOrientation
+        if (_deviceOrientation == OrientationEventListener.ORIENTATION_UNKNOWN) return 0
+        val sensorOrientation = cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) ?: 0
+
+        // Round device orientation to a multiple of 90
+        _deviceOrientation = (_deviceOrientation + 45) / 90 * 90
+
+        // Reverse device orientation for front-facing cameras
+        val facingFront =
+            cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT
+        if (facingFront) _deviceOrientation = -_deviceOrientation
+
+        // Calculate desired JPEG orientation relative to camera orientation to make
+        // the image upright relative to the device orientation
+        return (sensorOrientation + _deviceOrientation + 360) % 360
     }
 
     private fun rotateImage(img: Bitmap, degree: Int): Bitmap {
