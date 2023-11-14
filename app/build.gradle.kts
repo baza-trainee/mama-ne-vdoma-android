3bca9d242386c220b8067008f55e7830d98a3488
o.krasnonozhenko <o.krasnonozhenko@andersenlab.com>
2023-11-14 08:03:53 +0200

    Version 0.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "tech.baza_trainee.mama_ne_vdoma"
         minSdk = 24
         targetSdk = 33
-        versionCode = 33
-        versionName = "0.4.3"
+        versionCode = 34
+        versionName = "0.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
