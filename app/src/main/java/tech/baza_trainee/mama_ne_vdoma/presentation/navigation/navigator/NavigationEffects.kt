ff8d140f6455a7f25e0c26a4c1e69ed29dca4acb
o.krasnonozhenko <o.krasnonozhenko@andersenlab.com>
2024-01-30 11:38:24 +0200

    Navigate back only if current screen is resumed

--- app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/navigation/navigator/NavigationEffects.kt
+++ app/src/main/java/tech/baza_trainee/mama_ne_vdoma/presentation/navigation/navigator/NavigationEffects.kt
@@ -3,11 +3,11 @@ package tech.baza_trainee.mama_ne_vdoma.presentation.navigation.navigator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 import tech.baza_trainee.mama_ne_vdoma.presentation.utils.extensions.findActivity
-
 
 @Composable
 fun NavigationEffects(
@@ -21,7 +21,11 @@ fun NavigationEffects(
                 return@collect
             }
             when (intent) {
-                is NavigationIntent.NavigateBack -> navHostController.popBackStack()
+                is NavigationIntent.NavigateBack -> {
+                    if (navHostController.currentBackStackEntry?.lifecycle?.currentState ==
+                        Lifecycle.State.RESUMED)
+                        navHostController.popBackStack()
+                }
 
                 is NavigationIntent.NavigateTo -> {
                     navHostController.navigate(intent.route.route)
