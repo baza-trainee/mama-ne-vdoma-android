f7e59afbffee83a652e29729156daa7c381c3a87
o.krasnonozhenko <o.krasnonozhenko@andersenlab.com>
2024-04-22 07:17:44 +0300

    Version 41 -> 42

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "tech.baza_trainee.mama_ne_vdoma"
         minSdk = 24
         targetSdk = 34
-        versionCode = 51
+        versionCode = 52
         versionName = "0.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
